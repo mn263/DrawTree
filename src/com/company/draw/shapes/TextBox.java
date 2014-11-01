73925f8b1d8ecc59095ca2bac42821a48b616e0e
Matt <matt.nielson.byu@gmail.com>
2014-11-01 15:32:59 -0600

    Improved null Root handling.

--- src/com/company/draw/shapes/TextBox.java
+++ src/com/company/draw/shapes/TextBox.java
@@ -4,9 +4,14 @@ import com.company.*;
 import spark.data.*;
 import sun.reflect.generics.reflectiveObjects.*;
 
+import java.awt.*;
 import java.awt.geom.*;
+import java.util.*;
 
-public class TextBox implements Interactable {
+import static com.company.draw.shapes.WidgetUtils.handleMouse;
+import static com.company.draw.shapes.WidgetUtils.mouseType.UP;
+
+public class TextBox extends SOReflect implements ModelListener, Drawable, Interactable {
 
 	public String state;
 	public SA contents;
@@ -15,7 +20,13 @@ public class TextBox implements Interactable {
 	public SO active;
 	public SA model;
 
+	private Text content = null;
 
+	public TextBox() {
+		WidgetUtils.addListener(this);
+	}
+
+//	INTERACTABLE
 	@Override
 	public Root getPanel() {
 		throw new NotImplementedException();
@@ -28,16 +39,92 @@ public class TextBox implements Interactable {
 
 	@Override
 	public boolean mouseDown(double x, double y, AffineTransform myTransform) {
-		throw new NotImplementedException();
+		return callHandleMouse(WidgetUtils.mouseType.DOWN, x, y, myTransform);
 	}
 
 	@Override
 	public boolean mouseMove(double x, double y, AffineTransform myTransform) {
-		throw new NotImplementedException();
+		return callHandleMouse(WidgetUtils.mouseType.MOVE, x, y, myTransform);
 	}
 
 	@Override
 	public boolean mouseUp(double x, double y, AffineTransform myTransform) {
-		throw new NotImplementedException();
+		return callHandleMouse(UP, x, y, myTransform);
+	}
+
+	private boolean callHandleMouse(WidgetUtils.mouseType mouseType, double x, double y, AffineTransform myTransform) {
+		if (content == null) loadContentText();
+		boolean isHandled = handleMouse(contents, x, y, myTransform, mouseType);
+		if (!isHandled) {
+			this.state = "idle";
+			changeState(this.idle, mouseType);
+		} else {
+			if (WidgetUtils.getMouseStatus() == WidgetUtils.MouseStatus.PRESSED) {
+				this.state = "active";
+				changeState(this.active, mouseType);
+				if (content.select(x, y, 0, myTransform) != null) {
+					content.mouseDown(x, y, myTransform);
+				}
+			} else {
+				this.state = "hover";
+				changeState(this.hover, mouseType);
+			}
+		}
+		return isHandled;
+	}
+
+	private void loadContentText() {
+		for (int i = 0; i < contents.size(); i++) {
+			SO so = contents.get(i).getSO();
+			Selectable selectable = (Selectable) so;
+			if (so.get("class") != null && "\"content\"".equals(so.get("class").toString())) {
+				if (selectable.getClass().toString().equals("class com.company.draw.shapes.Text")) {
+					this.content = (Text) selectable;
+				}
+			}
+		}
+	}
+
+	public void changeState(SO newState, WidgetUtils.mouseType mouseType) {
+		for (int i = 0; i < contents.size(); i++) {
+			SO so = contents.get(i).getSO();
+			Selectable selectable = (Selectable) so;
+			if (so.get("class") != null && "\"active\"".equals(so.get("class").toString())) {
+				selectable.setBackgroundColor(newState);
+			}
+		}
+	}
+
+
+//	DRAWABLE
+	@Override
+	public void paint(Graphics g) {
+		int cSize = contents.size();
+		Graphics2D g2 = (Graphics2D) g;
+		for (int i = 0; i < cSize; i++) {
+			callPaintOnContents(contents.get(i), g2);
+		}
+	}
+
+	public void callPaintOnContents(SV sv, Graphics g) {
+		SO so = sv.getSO();
+		Drawable drawable = (Drawable) so;
+		drawable.paint(g);
+	}
+
+
+	// MODEL LISTENER
+	@Override
+	public void modelUpdated(ArrayList<String> modelPath, String newValue) {
+		if(content == null) loadContentText();
+
+		if (modelPath.size() == model.size()) {
+			for (int i = 0; i < model.size(); i++) {
+				if (!modelPath.get(i).equals(model.getString(i))) {
+					return; //IT WASN'T A MATCH
+				}
+			}
+			content.text = newValue;
+		}
 	}
 }
