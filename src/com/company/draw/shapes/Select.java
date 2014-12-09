9a7960f28a5e1a3c1c4c44e6c69778c5ab93a126
Matt <matt.nielson.byu@gmail.com>
2014-12-09 11:43:33 -0700

    Fixed Selection (when deep).

--- src/com/company/draw/shapes/Select.java
+++ src/com/company/draw/shapes/Select.java
@@ -1,6 +1,5 @@
 package com.company.draw.shapes;
 
-import com.company.Point;
 import com.company.*;
 import spark.data.*;
 
@@ -11,8 +10,7 @@ import java.util.*;
 public class Select extends Group implements Selectable {
 
 	//When selected changes we should call repaint
-	private Selectable selected = null;
-	private AffineTransform totalTransform = null;
+	private ArrayList<Integer> selected = new ArrayList<>();
 
 	public Select(){super();}
 	public Select(Group group) {
@@ -21,66 +19,59 @@ public class Select extends Group implements Selectable {
 
 
 	public void paintSelected(Graphics g) {
-		if (selected == null || totalTransform == null) return;
+		if (selected.size() <= 0) return;
+		SV sv = null;
+		AffineTransform forwardTransform = WidgetUtils.getBackwardsTransform(tx, ty, sx, sy, rotate);
+		for (int i = selected.size() - 2; i >= 0; i--) {
+			int index = selected.get(i);
+			if (sv == null) {
+				sv = contents.get(index);
+			} else {
+				SO so = sv.getSO();
+				Selectable selectable = (Selectable) so;
+				if (selectable instanceof Group) {
+					Group group = (Group) selectable;
+					sv = group.contents.get(index);
+				}
+			}
+			SO so = sv.getSO();
+			Selectable selectable = (Selectable) so;
+			if (selectable instanceof Group) {
+				Group group = (Group) selectable;
+				AffineTransform temp = WidgetUtils.getBackwardsTransform(group.tx, group.ty, group.sx, group.sy, group.rotate);
+				forwardTransform.concatenate(temp);
+			}
+		}
+		if (sv == null) return;
+		SO so = sv.getSO();
+		Selectable selectable = (Selectable) so;
+		Point2D[] controlPoints = selectable.controls();
+
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.darkGray);
-
 		AffineTransform gTrans = g2.getTransform();
-		try {
-			gTrans.preConcatenate(totalTransform.createInverse());
-		} catch (NoninvertibleTransformException e) {
-			e.printStackTrace();
-		}
+		gTrans.concatenate(forwardTransform);
 
-		Point2D[] controlPoints = selected.controls();
 		double offY = g2.getTransform().getTranslateY();
 		for (Point2D point : controlPoints) {
-			point = new Point(point.getX(), point.getY() - offY);
 			Point2D ptDst = gTrans.transform(point, null);
-			g2.fillRect((int) ptDst.getX(), (int) (ptDst.getY()), 4, 4);
+			g2.fillRect((int) ptDst.getX(), (int) (ptDst.getY() - offY), 4, 4);
 		}
 	}
 
-
 	@Override
-	public ArrayList<Integer> select(double x, double y, int myIndex, AffineTransform oldTrans) {
-
-		AffineTransform transform = WidgetUtils.getTransform(tx, ty, sx, sy, rotate);
-		// Add on old transform
-		transform.concatenate(oldTrans);
-
-		for (int i = contents.size() - 1; i >= 0; i--) {
-			SV sv = contents.get(i);
-			SO so = sv.getSO();
-			if (!(so instanceof Selectable)) continue;
-			Selectable selectable = (Selectable) so;
-			ArrayList<Integer> path = selectable.select(x, y, i, transform);
-			if (path != null) {
-				this.selected = selectable;
-				this.totalTransform = transform;
-				TreePanel.selected = this;
-				return null;
-			}
+	public ArrayList<Integer> select(double x, double y, int myIndex, AffineTransform transform) {
+		ArrayList<Integer> path = super.select(x, y, myIndex, transform);
+		if (path != null) {
+			this.selected = path;
+			SwingTree.treePanel.repaint();
 		}
-		return null;
+		return path;
 	}
 
 	@Override
 	public Point2D[] controls() {
 		return new Point2D[0];
 	}
-
-
-	@Override
-	public boolean mouseUp(double x, double y, AffineTransform myTransform) {
-		boolean handled = super.mouseUp(x, y, myTransform);
-		if (handled) {
-			this.select(x, y, 0, myTransform);
-		}
-		return handled;
-	}
-
-	@Override
-	public void makeIdle() { }
 
 }
